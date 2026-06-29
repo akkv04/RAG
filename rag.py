5c956d342d82d499e50ae1a7854d80e67499bb4b
Akhil KV <akhilkv04@gmail.com>
2026-06-29 18:18:05 +1000

    Cleanup

--- rag.py
+++
@@ -1,6 +0,0 @@
-import pypdf2
-import chromadb
-import uuid
-
-chroma_client = chromadb.PersistentClient(path = "./chromdadb")
-collection = chroma_client.get_or_create_collection(name="test_collection")
